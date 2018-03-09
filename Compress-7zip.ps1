8037c658eedf2319375236ca4f818efc4a646849
Dylan Burton <dburton@corporate.enkon>
2018-03-09 13:01:12 -0800

    Remove hashtable idea and add switch for multicore

--- Compress-7zip.ps1
+++ Compress-7zip.ps1
@@ -1,29 +1,30 @@
-﻿function Compress-7Zip {
+﻿function Compress-Escrow {
     [CmdletBinding()]
     param (
-        [String]$Path = 'C:\Users\dburton\Documents\scripts\' ,
-        [String]$Target = 'C:\Users\dburton\Desktop\escrow.7zip',
-        [string]$VolumeSize = '4400000k'
-        #[String]$WorkingDirectory = 'D:\escrow\'
+        [String]$Path,
+        [String]$Target,
+        [string]$VolumeSize,
+        [switch]$EnableMultiCore
     )
 
+    begin {
+        if (-not (test-path "$env:ProgramFiles\7-Zip\7z.exe")) {throw "$env:ProgramFiles\7-Zip\7z.exe needed"} 
+        set-alias 7z "$env:ProgramFiles\7-Zip\7z.exe"
 
-    if (-not (test-path "$env:ProgramFiles\7-Zip\7z.exe")) {throw "$env:ProgramFiles\7-Zip\7z.exe needed"} 
-    set-alias 7z "$env:ProgramFiles\7-Zip\7z.exe"
+        $Password = Read-Host -Prompt "Enter Password"
+        $Password = "-p$Password"
 
-    $Password = Read-Host -Prompt "Enter Password"
-    $Password = "-p$Password"
-
-    $NumCores = Get-WmiObject –class Win32_processor | select -ExpandProperty numberofcores
-    $MultiThread = "-mmt$NumCores"
-
-    $7zipParamaters = @{
-        Password = "-p$Password";
-        #Path = $Path;
-        #Format = 'SevenZip';
-        #CompressionLevel = 'Normal';
-        #CustomInitialization = $InitScript;
+        if ($EnableMultiCore) {
+            $NumCores = Get-WmiObject –class Win32_processor | select -ExpandProperty numberofcores
+            $MultiThread = "-mmt$NumCores"
+        }
+    
     }
 
-    7z a $Target $Path -r -v4400m -t7z -m0=LZMA2 $MultiThread ($7zipParamaters.Values | Out-String) #$Password -mhe
+
+    process {
+        7z a $Target $Path -r -v4400m -t7z -m0=LZMA2 $MultiThread $Password
+    }
+
+    end {}
 }
